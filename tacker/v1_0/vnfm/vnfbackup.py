072eaec77acbbb45b4383b11bfd43a04dcf628f8
Shinewisely <delightwook@dcn.ssu.ac.kr>
2017-07-03 20:51:57 +0900

    tacker-freezer client

---
+++ tacker/v1_0/vnfm/vnfbackup.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import print_function
+
+import yaml
+
+from oslo_serialization import jsonutils
+
+from tackerclient.i18n import _
+from tackerclient.tacker import v1_0 as tackerV10
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tackerclient.tacker import v1_0 as tackerV10
+
+_VNFBACKUP = 'vnfbackup'
+
+class ListBackup(tackerV10.ListCommand):
+    resource = _VNFBACKUP
+    list_columns = ['Vnf_id','Job_id','Action']
